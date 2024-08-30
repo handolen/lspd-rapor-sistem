ce920e1a7136933d4363b6c45a6a40b5db54253c
Yunus Emre Kaya <emrkyy@gmail.com>
2024-08-30 15:11:55 +0300

    elkoyma-rapor

---
+++ elkoyma/app.js
@@ -0,0 +1,59 @@
+$('.ui.radio.checkbox').checkbox();
+
+$('#submit').click(function () {
+    let output = `
+<div class="content">
+    <div align="center">
+        <span style="color:Transparent">aaa</span><br>
+        <center>
+            <p>##</p>
+            <img style="height: 100px;" src="https://i.hizliresim.com/j7lwfrk.png" alt="Image">
+            <p style="color: grey; font-size: 100%;">LOS SANTOS POLICE DEPARTMENT</p>
+            <p style="color: black; font-size: 150%; font-weight: bold;">EL KOYMA RAPORU</p>
+        </center>
+        <table style="font-family: Arial; width: 100%;">
+            <tr>
+                <td valign="top" style="border-top:1px solid #111;border-right:1px solid #111;border-left:1px solid #111;border-bottom:1px solid #111;padding:3px;padding-left:6px;margin:1px;border-radius:2px"><strong class="text-strong">OLAY TARİHİ</strong></td>
+                <td valign="top" style="border-top:1px solid #111;border-right:1px solid #111;border-left:1px solid #111;border-bottom:1px solid #111;padding:3px;padding-left:6px;margin:1px;border-radius:2px"><strong class="text-strong">KONUM</strong></td>            </tr>
+            <tr>
+                <td valign="top" style="border-top:1px solid #111;border-right:1px solid #111;border-left:1px solid #111;border-bottom:1px solid #111;padding:3px;padding-left:6px;margin:1px;border-radius:2px">${$('input[name="tarih"]').val()}</td>
+                <td valign="top" style="border-top:1px solid #111;border-right:1px solid #111;border-left:1px solid #111;border-bottom:1px solid #111;padding:3px;padding-left:6px;margin:1px;border-radius:2px">${$('input[name="konum"]').val()}</td>
+            </tr>
+        </table>
+
+        <table style="font-family: Arial; width: 100%;">
+            <tr>
+                <td valign="top" style="border-top:1px solid #111;border-right:1px solid #111;border-left:1px solid #111;border-bottom:1px solid #111;padding:3px;padding-left:6px;margin:1px;border-radius:2px"><strong class="text-strong">EL KOYAN MEMUR</strong></td>
+                <td valign="top" style="border-top:1px solid #111;border-right:1px solid #111;border-left:1px solid #111;border-bottom:1px solid #111;padding:3px;padding-left:6px;margin:1px;border-radius:2px"><strong class="text-strong">VARSA PARTNER ISMI</strong></td>
+                <td valign="top" style="border-top:1px solid #111;border-right:1px solid #111;border-left:1px solid #111;border-bottom:1px solid #111;padding:3px;padding-left:6px;margin:1px;border-radius:2px"><strong class="text-strong">BİRİM KODU</strong></td>
+            </tr>
+            <tr>
+                <td valign="top" style="border-top:1px solid #111;border-right:1px solid #111;border-left:1px solid #111;border-bottom:1px solid #111;padding:3px;padding-left:6px;margin:1px;border-radius:2px">${$('input[name="birincimemur"]').val()}</td>
+                <td valign="top" style="border-top:1px solid #111;border-right:1px solid #111;border-left:1px solid #111;border-bottom:1px solid #111;padding:3px;padding-left:6px;margin:1px;border-radius:2px">${$('input[name="ikincimemur"]').val()}</td>
+                <td valign="top" style="border-top:1px solid #111;border-right:1px solid #111;border-left:1px solid #111;border-bottom:1px solid #111;padding:3px;padding-left:6px;margin:1px;border-radius:2px">${$('input[name="birimkodu"]').val()}</td>
+            </tr>
+        </table>
+
+
+        <table style="font-family: Arial; width: 100%;">
+            <tr>
+                <td valign="top" style="border-top:1px solid #111;border-right:1px solid #111;border-left:1px solid #111;border-bottom:1px solid #111;padding:3px;padding-left:6px;margin:1px;border-radius:2px"><strong class="text-strong">EL KOYULAN KİŞİ</strong></td>
+                <td valign="top" style="border-top:1px solid #111;border-right:1px solid #111;border-left:1px solid #111;border-bottom:1px solid #111;padding:3px;padding-left:6px;margin:1px;border-radius:2px"><strong class="text-strong">EL KOYULAN MATERYAL</strong></td>
+                <td valign="top" style="border-top:1px solid #111;border-right:1px solid #111;border-left:1px solid #111;border-bottom:1px solid #111;padding:3px;padding-left:6px;margin:1px;border-radius:2px"><strong class="text-strong">MİKTAR</strong></td>
+            </tr>
+            <tr>
+                <td valign="top" style="border-top:1px solid #111;border-right:1px solid #111;border-left:1px solid #111;border-bottom:1px solid #111;padding:3px;padding-left:6px;margin:1px;border-radius:2px">${$('input[name="tutuklanan"]').val()} </td>
+                <td valign="top" style="border-top:1px solid #111;border-right:1px solid #111;border-left:1px solid #111;border-bottom:1px solid #111;padding:3px;padding-left:6px;margin:1px;border-radius:2px">${$('input[name="tutuklanmasebebi"]').val()}</td>
+                <td valign="top" style="border-top:1px solid #111;border-right:1px solid #111;border-left:1px solid #111;border-bottom:1px solid #111;padding:3px;padding-left:6px;margin:1px;border-radius:2px">${$('input[name="hapissuresi"]').val()}</td>
+            </tr>
+        </table>
+        
+        <p>##</p>
+    </div>
+</body>
+</html>
+    `;
+
+    $('#output').html(output);
+
+});
